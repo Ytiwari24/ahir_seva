23ef5700a4c358bb5a20eb2b01571ba383e7c934
Pravind <parvindraj004@gmail.com>
2023-08-09 18:21:59 +0530

    added family , frequent visitor

---
+++ db.js
@@ -0,0 +1,17 @@
+// db.js
+
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'sicurezza_user_app',
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
+});
+
+module.exports = db;
