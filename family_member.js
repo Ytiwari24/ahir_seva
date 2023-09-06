c05e357a74086746bc74ab0a0919a3aba46dc8e7
Pravind <parvindraj004@gmail.com>
2023-09-06 16:25:29 +0530

    dymanic api

--- family_member.js
+++ family_member.js
@@ -2,28 +2,92 @@ const express = require('express')
 const jwt = require('jsonwebtoken');
 const app = express.Router()
 const multer = require('multer');
-// const { verifyToken } = require('./middle_ware');
 const db = require('./db');
 
-
+const SECRET_KEY = 'your_secret_key';
 
 const upload = multer({ dest: 'uploads/' });
 // ***********GET FAMILY MEMBERS ********** //
-// Get family members for a specific user by user ID
-app.get('/members/:userId', (req, res) => {
-    const userId = req.params.userId;
-    const sql = 'SELECT * FROM family_member WHERE user_id = ?';
-  
-    db.query(sql, [userId], (err, results) => {
-      if (err) {
-        console.error('Error fetching family members:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
+// Get Family Members (requires authentication)
+app.get('/members', (req, res) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        res.status(401).json({ message: 'Authentication required' });
         return;
-      }
-  
-      res.json(results);
+    }
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            res.status(401).json({ message: 'Authentication failed' });
+            return;
+        }
+        const userId = decoded.userId;
+        db.query(
+            'SELECT * FROM family_member WHERE user_id = ?',
+            [userId],
+            (dbErr, results) => {
+                if (dbErr) {
+                    console.error('Error querying the database:', dbErr);
+                    res.status(500).json({ message: 'Internal server error' });
+                    return;
+                }
+                res.status(200).json(results);
+            }
+        );
     });
-  });
+});
+
+// Get all Parents members API
+app.get('/parents', (req, res) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        res.status(401).json({ message: 'Authentication required' });
+        return;
+    }
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            res.status(401).json({ message: 'Authentication failed' });
+            return;
+        }
+        const userId = decoded.userId;
+        db.query(
+            'SELECT id, first_name, last_name FROM family_member WHERE user_id = ?',
+            [userId],
+            (dbErr, results) => {
+                if (dbErr) {
+                    console.error('Error querying the database:', dbErr);
+                    res.status(500).json({ message: 'Internal server error' });
+                    return;
+                }
+                res.status(200).json(results);
+            }
+        );
+    });
+});
+// app.get('/parents', (req, res) => {
+    
+//     // Retrieve all family members from the database
+//     const sql = 'SELECT id, first_name, last_name FROM family_member';
+//     db.query(sql, (err, results) => {
+//         if (err) throw err;
+//         res.json(results);
+//     });
+// });
+
+// Get family members for a specific user by user ID
+// app.get('/members', (req, res) => {
+//     const userId = req.params.userId;
+//     const sql = 'SELECT * FROM family_member WHERE user_id = ?';
+
+//     db.query(sql, [userId], (err, results) => {
+//       if (err) {
+//         console.error('Error fetching family members:', err);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//         return;
+//       }
+
+//       res.json(results);
+//     });
+//   });
 // app.get('/members/:userId', (req, res) => {
 //     userID = req.params.id;
 //     const query = 'SELECT * FROM family_member WHERE user_id= ?';
@@ -38,34 +102,72 @@ app.get('/members/:userId', (req, res) => {
 // });
 
 
-// Get all Parents members API
-app.get('/parents', (req, res) => {
-    // Retrieve all family members from the database
-    const sql = 'SELECT id, first_name, last_name FROM family_member';
-    db.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json(results);
-    });
-});
 
-// *************ADD FAMILY MEMBER**************** //
-app.post('/add',(req, res) => {
-    const { parent_id, first_name, last_name, mobile_no, dob, gender, relation, profession } = req.body;
-    // const imageData = req.file.buffer;
 
-    const query = 'INSERT INTO family_member (parent_id,first_name, last_name, mobile_no, dob, gender, relation, profession) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-    const values = [parent_id, first_name, last_name, mobile_no, dob, gender, relation, profession];
 
-    db.query(query, values, (err, result) => {
+
+///*******************ADD MEMBER WITH AUTHORIZATION********************************///
+// Add Family Member
+app.post('/add', (req, res) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+        res.status(401).json({ message: 'Authentication required' });
+        return;
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
         if (err) {
-            console.error('Error adding family member:', err);
-            res.status(500).json({ error: 'Failed to add family member' });
-        } else {
-            console.log('Family member added:', result);
-            res.json({ message: 'Family member added successfully' });
+            res.status(401).json({ message: 'Authentication failed' });
+            return;
         }
+
+        const userId = decoded.userId;
+
+        // Parse data from the request body
+        const { parent_id, first_name, last_name, mobile_no, dob, gender, relation, profession } = req.body;
+
+        // Validate input data (ensure name and relationship are provided)
+        //   if (!name || !relationship) {
+        // res.status(400).json({ message: 'Name and relationship are required' });
+        // return;
+        //   }
+
+        // Insert the new family member into the database
+        const query = 'INSERT INTO family_member (user_id,parent_id,first_name, last_name, mobile_no, dob, gender, relation, profession) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)';
+        const values = [userId, parent_id, first_name, last_name, mobile_no, dob, gender, relation, profession];
+        db.query(query, values,
+            (dbErr, results) => {
+                if (dbErr) {
+                    console.error('Error inserting family member:', dbErr);
+                    res.status(500).json({ message: 'Internal server error' });
+                    return;
+                }
+
+                res.status(201).json({ message: 'Family member added successfully' });
+            }
+        );
     });
 });
+
+// // *************ADD FAMILY MEMBER**************** //
+// app.post('/add', (req, res) => {
+//     const { parent_id, first_name, last_name, mobile_no, dob, gender, relation, profession } = req.body;
+//     // const imageData = req.file.buffer;
+
+//     const query = 'INSERT INTO family_member (parent_id,first_name, last_name, mobile_no, dob, gender, relation, profession) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+//     const values = [parent_id, first_name, last_name, mobile_no, dob, gender, relation, profession];
+
+//     db.query(query, values, (err, result) => {
+//         if (err) {
+//             console.error('Error adding family member:', err);
+//             res.status(500).json({ error: 'Failed to add family member' });
+//         } else {
+//             console.log('Family member added:', result);
+//             res.json({ message: 'Family member added successfully' });
+//         }
+//     });
+// });
 
 // Update a family member
 app.put('/update/:id', (req, res) => {
@@ -80,6 +182,100 @@ app.put('/update/:id', (req, res) => {
         } else {
             res.json({ message: 'Family member updated successfully' });
         }
+    });
+});
+
+///***************UPDATE MEMBER WITH AUTHORIZATION //IMPLEMENTATION IS PENDING//**********************///
+// Update Family Member
+app.put('/family-members/:id', (req, res) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+        res.status(401).json({ message: 'Authentication required' });
+        return;
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            res.status(401).json({ message: 'Authentication failed' });
+            return;
+        }
+
+        const userId = decoded.userId;
+        const memberId = req.params.id; // Extract member ID from the URL parameter
+
+        // Parse data from the request body
+        const { name, relationship } = req.body;
+
+        // Validate input data (ensure name and relationship are provided)
+        if (!name || !relationship) {
+            res.status(400).json({ message: 'Name and relationship are required' });
+            return;
+        }
+
+        // Update the family member in the database
+        db.query(
+            'UPDATE family_members SET name = ?, relationship = ? WHERE id = ? AND user_id = ?',
+            [name, relationship, memberId, userId],
+            (dbErr, results) => {
+                if (dbErr) {
+                    console.error('Error updating family member:', dbErr);
+                    res.status(500).json({ message: 'Internal server error' });
+                    return;
+                }
+
+                if (results.affectedRows === 0) {
+                    res.status(404).json({ message: 'Family member not found' });
+                    return;
+                }
+
+                res.status(200).json({ message: 'Family member updated successfully' });
+            }
+        );
+    });
+});
+
+
+
+
+///********************DELETE MEMBER WITH AUTHORIZATION //IMPLEMENTATION IS PENDING//*********************************///
+// Delete Family Member
+app.delete('/family-members/:id', (req, res) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+        res.status(401).json({ message: 'Authentication required' });
+        return;
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            res.status(401).json({ message: 'Authentication failed' });
+            return;
+        }
+
+        const userId = decoded.userId;
+        const memberId = req.params.id; // Extract member ID from the URL parameter
+
+        // Delete the family member from the database
+        db.query(
+            'DELETE FROM family_members WHERE id = ? AND user_id = ?',
+            [memberId, userId],
+            (dbErr, results) => {
+                if (dbErr) {
+                    console.error('Error deleting family member:', dbErr);
+                    res.status(500).json({ message: 'Internal server error' });
+                    return;
+                }
+
+                if (results.affectedRows === 0) {
+                    res.status(404).json({ message: 'Family member not found' });
+                    return;
+                }
+
+                res.status(200).json({ message: 'Family member deleted successfully' });
+            }
+        );
     });
 });
 
--- login/login.js
+++ login/login.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 // const { verifyToken } = require('./middle_ware');
 const db = require('../db');
 // const routes=require('./')
-
+const SECRET_KEY = 'your_secret_key';
 ///********************TRY NEW LOGIN USERS AND GET DATA******************///
 
 ///************FOR CREATING NEW USERS**************///
@@ -50,33 +50,147 @@ app.get('/users', (req, res) => {
 ///***************LOGIN USERS********************///
 // 5. Implement Login API
 
-// 1. Implement Login API
-app.post('/login', async (req, res) => {
+// User Login
+app.post('/login', (req, res) => {
   const { email, password } = req.body;
 
-  const query = `SELECT * FROM users WHERE email = ?`;
-  db.query(query, [email], async (err, results) => {
+  db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
     if (err) {
-      console.error('Error logging in:', err);
-      res.status(500).json({ error: 'Error logging in' });
-    } else {
-      if (results.length === 1) {
-        const user = results[0];
-
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (isPasswordValid) {
-          const token = jwt.sign({ userId: user.id }, 'your_secret_key'); // Replace with a secret key
-          res.json({ token, user: { ...user, password: undefined }, });
-        } else {
-          res.status(401).json({ error: 'Invalid credentials' });
-        }
-      } else {
-        res.status(401).json({ error: 'Invalid credentials' });
-      }
+      console.error('Error querying the database:', err);
+      res.status(500).json({ message: 'Internal server error' });
+      return;
     }
+
+    if (results.length === 0) {
+      res.status(401).json({ message: 'Authentication failed' });
+      return;
+    }
+
+    const user = results[0];
+
+    bcrypt.compare(password, user.password, (bcryptErr, bcryptResult) => {
+      if (bcryptErr || !bcryptResult) {
+        res.status(401).json({ message: 'Authentication failed' });
+        return;
+      }
+
+      const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
+        expiresIn: '1h',
+      });
+
+      res.status(200).json({ token });
+    });
   });
 });
+
+///*********************FAMILY MEMBER*************************///
+
+// // Get Family Members (requires authentication)
+// app.get('/family-members', (req, res) => {
+//   const token = req.header('Authorization')?.replace('Bearer ', '');
+
+//   if (!token) {
+//     res.status(401).json({ message: 'Authentication required' });
+//     return;
+//   }
+
+//   jwt.verify(token, SECRET_KEY, (err, decoded) => {
+//     if (err) {
+//       res.status(401).json({ message: 'Authentication failed' });
+//       return;
+//     }
+
+//     const userId = decoded.userId;
+
+//     db.query(
+//       'SELECT * FROM family_member WHERE user_id = ?',
+//       [userId],
+//       (dbErr, results) => {
+//         if (dbErr) {
+//           console.error('Error querying the database:', dbErr);
+//           res.status(500).json({ message: 'Internal server error' });
+//           return;
+//         }
+
+//         res.status(200).json(results);
+//       }
+//     );
+//   });
+// });
+
+// ///*******************ADD MEMBER API********************************///
+// // Add Family Member
+// app.post('/family-members', (req, res) => {
+//   const token = req.header('Authorization')?.replace('Bearer ', '');
+
+//   if (!token) {
+//     res.status(401).json({ message: 'Authentication required' });
+//     return;
+//   }
+
+//   jwt.verify(token, SECRET_KEY, (err, decoded) => {
+//     if (err) {
+//       res.status(401).json({ message: 'Authentication failed' });
+//       return;
+//     }
+
+//     const userId = decoded.userId;
+
+//     // Parse data from the request body
+//     const { name, relationship } = req.body;
+
+//     // Validate input data (ensure name and relationship are provided)
+//     if (!name || !relationship) {
+//       res.status(400).json({ message: 'Name and relationship are required' });
+//       return;
+//     }
+
+//     // Insert the new family member into the database
+//     db.query(
+//       'INSERT INTO family_member (user_id, first_name, last_name) VALUES (?, ?, ?)',
+//       [userId, name, relationship],
+//       (dbErr, results) => {
+//         if (dbErr) {
+//           console.error('Error inserting family member:', dbErr);
+//           res.status(500).json({ message: 'Internal server error' });
+//           return;
+//         }
+
+//         res.status(201).json({ message: 'Family member added successfully' });
+//       }
+//     );
+//   });
+// });
+
+
+///***********************************************************///
+// 1. Implement Login API
+// app.post('/login', async (req, res) => {
+//   const { email, password } = req.body;
+
+//   const query = `SELECT * FROM users WHERE email = ?`;
+//   db.query(query, [email], async (err, results) => {
+//     if (err) {
+//       console.error('Error logging in:', err);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     } else {
+//       if (results.length === 1) {
+//         const user = results[0];
+
+
+//         const isPasswordValid = await bcrypt.compare(password, user.password);
+//         if (isPasswordValid) {
+//           const token = jwt.sign({ userId: user.id }, 'your_secret_key'); // Replace with a secret key
+//           res.json({ token, user: { ...user, password: undefined }, });
+//         } else {
+//           res.status(401).json({ error: 'Invalid credentials' });
+//         }
+//       } else {
+//         res.status(401).json({ error: 'Invalid credentials' });
+//       }
+//     }
+//   });
+// });
 
 
 
